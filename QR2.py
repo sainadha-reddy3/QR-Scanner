c67d2e33a11bf7efaad6f1113e7259f00df4f72f
sainadha-reddy3 <158275434+sainadha-reddy3@users.noreply.github.com>
2024-04-25 22:37:12 +0530

    Add files via upload

---
+++ QR2.py
@@ -0,0 +1,10 @@
+import qrcode
+from PIL import Image
+import qrcode.constants
+qr = qrcode.QRcode(version=1,
+                   error_correction=qrcode.constants.ERROR_CORRECT_H,
+                   box_size=10,border=4,)
+qr.add_data("https://movies2watch.fun/")
+qr.make(fit=True)
+img=qr.make_image(fill_color="red",back_color="blue")
+img.save("movies2watch.png")
